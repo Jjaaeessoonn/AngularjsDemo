32b7fc8c11b87c9ceacacc17a0a4371691b18e3a
Jason <jsn.lyy@gmail.com>
2018-01-31 16:06:00 -0500

    2nd commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin')
-	cleanCSS = require('gulp-clean-css'),
-	browserSync = require('browser-sync');
+	imagemin = require('gulp-imagemin'),
+	//cleanCSS = require('gulp-clean-css'),
+	sass = require('gulp-ruby-sass'),
+	browserSync = require('browser-sync').create();
 
-gulp.task('uglify', function(){
 
+gulp.task('serve', ['styles'], function(){
+	browserSync.init({
+		server: "./"
+	});
+
+	gulp.watch("./scss/*.scss", ['styles']);
+	gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 
-gulp.task('default', ['uglify'], function() {
-	console.log("Hello World!");
+
+
+gulp.task('scripts', function(){
+	gulp.src('app/*.js')  //ask how to change this to include all children directories too
+	.pipe(uglify())
+	.pipe(gulp.dest('build/js'));
 });
+
+gulp.task('styles', function(){
+	return sass('scss/**/*.scss', {
+		style: 'compressed',
+	})
+	.on('error', console.error.bind(console))
+	.pipe(gulp.dest('css/'))
+	.pipe(browserSync.stream());
+});
+
+
+
+
+gulp.task('default', ['scripts', 'serve']);
